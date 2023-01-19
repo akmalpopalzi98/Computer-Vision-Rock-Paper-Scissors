7acc5c04176e75b228d7ccc34f4565e51f936eb5
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-01-19 11:46:27 +0000

    adding new code

--- manual_rps.py
+++ manual_rps.py
@@ -10,3 +10,22 @@ def get_user_choice():
     return user_choice
 
 
+def get_winner(computer_choice, user_choice):
+    if computer_choice == 'Rock' and user_choice == 'Rock':
+        print('it is a tie!')
+    elif computer_choice == 'Rock' and user_choice == 'Paper':
+        print('You won!')
+    elif computer_choice == 'Rock' and user_choice == 'Scissors':
+        print('You lost!')
+    elif computer_choice == 'Paper' and user_choice == 'Paper':
+        print('Its a tie!')
+    elif computer_choice == 'Paper' and user_choice == 'Rock':
+        print('You lost!')
+    elif computer_choice == 'Paper' and user_choice == 'Scissors':
+        print('You won!')
+    elif computer_choice == 'Scissors' and user_choice == 'Scissors':
+        print('Its a tie!')
+    elif computer_choice == 'Scissors' and user_choice == 'Paper':
+        print('You lost!')
+    elif computer_choice == 'Scissors' and user_choice == 'Rock':
+        print('You won!')
